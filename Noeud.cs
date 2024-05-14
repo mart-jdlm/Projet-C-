32f141ef6adb2e898031b822e92708aebf590385
mart-jdlm <123973206+mart-jdlm@users.noreply.github.com>
2024-05-14 23:21:05 +0200

    Ajout de deux classes
    permet de visualiser l'organigramme

---
+++ Noeud.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HUET_JOUBERT
+{
+    internal class Noeud
+    {
+        public Salarie Employe { get; set; }
+        public List<Noeud> Subordonnes { get; set; }
+
+        public Noeud(Salarie employe)
+        {
+            Employe = employe;
+            Subordonnes = new List<Noeud>();
+        }
+
+        // Méthode pour ajouter un subordonné
+        public void AjouterSubordonne(Noeud subordonne)
+        {
+            Subordonnes.Add(subordonne);
+        }
+
+        // Méthode pour retirer un subordonné
+        public void RetirerSubordonne(Noeud subordonne)
+        {
+            Subordonnes.Remove(subordonne);
+        }
+    }
+}
---
+++ Organigramme.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HUET_JOUBERT
+{
+    internal class Organigramme
+    {
+        public Noeud Racine { get; set; }
+
+        public Organigramme(Salarie employeRacine)
+        {
+            Racine = new Noeud(employeRacine);
+        }
+
+        // Méthode pour embaucher un employé et l'ajouter à l'organigramme
+        public void Embaucher(Salarie nouvelEmploye, Noeud manager)
+        {
+            Noeud nouveauNoeud = new Noeud(nouvelEmploye);
+            manager.AjouterSubordonne(nouveauNoeud);
+        }
+
+        // Méthode récursive pour trouver un employé dans l'organigramme
+        private Noeud TrouverNoeud(Noeud noeudCourant, Salarie employe)
+        {
+            if (noeudCourant.Employe == employe)
+            {
+                return noeudCourant;
+            }
+
+            foreach (Noeud sousNoeud in noeudCourant.Subordonnes)
+            {
+                Noeud noeudTrouve = TrouverNoeud(sousNoeud, employe);
+                if (noeudTrouve != null)
+                {
+                    return noeudTrouve;
+                }
+            }
+
+            return null;
+        }
+
+        // Méthode pour licencier un employé et le retirer de l'organigramme
+        public void Licencier(Salarie employe)
+        {
+            // Recherche de l'employé dans l'organigramme
+            Noeud noeud = TrouverNoeud(Racine, employe);
+            if (noeud != null)
+            {
+                // Retrait de l'employé de son manager
+                Noeud parent = TrouverParent(Racine, employe);
+                if (parent != null)
+                {
+                    parent.RetirerSubordonne(noeud);
+                }
+            }
+        }
+
+        // Méthode récursive pour trouver le parent d'un employé dans l'organigramme
+        private Noeud TrouverParent(Noeud noeudCourant, Salarie employe)
+        {
+            foreach (Noeud sousNoeud in noeudCourant.Subordonnes)
+            {
+                if (sousNoeud.Employe == employe)
+                {
+                    return noeudCourant;
+                }
+                else
+                {
+                    Noeud parent = TrouverParent(sousNoeud, employe);
+                    if (parent != null)
+                    {
+                        return parent;
+                    }
+                }
+            }
+
+            return null;
+        }
+        // Méthode pour afficher l'organigramme
+        public void AfficherOrganigramme(Noeud noeud, string prefixe = "")
+        {
+            if (noeud == null) return;
+
+            Console.WriteLine(prefixe + "└── " + noeud.Employe.Nom);
+
+            foreach (Noeud sousNoeud in noeud.Subordonnes)
+            {
+                AfficherOrganigramme(sousNoeud, prefixe + "    ");
+            }
+        }
+    }
+}
